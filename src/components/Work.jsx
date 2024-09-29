c0dc9281604a44f267db8115c7396de213a74692
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-09-29 17:14:44 -0400

    First commit

---
+++ src/components/Work.jsx
@@ -0,0 +1,79 @@
+  import React from 'react'
+  import workImg from '../assets/projects/workImg.jpg'
+  import Dice from '../assets/projects/dice.jpg'
+  import Weather from '../assets/projects/weather.jpg'
+
+  const Work = () => {
+    return (
+      <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+          <div  className='pb-8'> 
+            <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
+            <p className='py-6'>Check out some of my recent works</p>
+          </div>
+
+          {/* Container*/}
+          <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+
+                       {/*Grid Item */}
+
+                <div style={{ backgroundImage: `url(${workImg})` }}
+                className="shadow-lg shadow-[#040c16] group container rounded-md 
+                    flex justify-center text-center items-center mx-auto content-div ">
+                  
+                  {/*Hover Effect */}
+                  <div className="opacity-0 group-hover:opacity-100 ">
+                    <span className="text-2xl font bold text-white tracking-wider ">
+                        Workout Buddy
+                    </span>
+                    <div className="pt-8 text-center ">
+                      <a href='https://github.com/smaranbh7/Workout-app' target='_blank'>
+                        <button className="text-center rounded-lg px-4 py-3 m-2
+                            bg-white text-gray-700 font-bold text-lg">Code</button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div style={{ backgroundImage: `url(${Dice})` }}
+                className="shadow-lg shadow-[#040c16] group container rounded-md 
+                    flex justify-center text-center items-center mx-auto content-div ">
+                  
+                  {/*Hover Effect */}
+                  <div className="opacity-0 group-hover:opacity-100 ">
+                    <span className="text-2xl font bold text-white tracking-wider ">
+                        Dice Game
+                    </span>
+                    <div className="pt-8 text-center ">
+                      <a href='https://github.com/smaranbh7/DiceGame' target='_blank'>
+                        <button className="text-center rounded-lg px-4 py-3 m-2
+                            bg-white text-gray-700 font-bold text-lg">Code</button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                {/* Project 3*/}
+                <div style={{ backgroundImage: `url(${Weather})` }}
+                className="shadow-lg shadow-[#040c16] group container rounded-md 
+                    flex justify-center text-center items-center mx-auto content-div ">
+                  
+                  {/*Hover Effect */}
+                  <div className="opacity-0 group-hover:opacity-100 ">
+                    <span className="text-2xl font bold text-white tracking-wider ">
+                        Weather Application
+                    </span>
+                    <div className="pt-8 text-center ">
+                      <a href='https://github.com/smaranbh7/weather-app' target='_blank'>
+                        <button className="text-center rounded-lg px-4 py-3 m-2
+                            bg-white text-gray-700 font-bold text-lg">Code</button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+          </div>
+        </div>
+        
+      </div>
+    )
+  }
+
+  export default Work
