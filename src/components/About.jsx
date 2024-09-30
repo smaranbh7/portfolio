1e0fcb8f277f71ce3b431eb054b113fea2efbb25
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-09-29 22:44:12 -0400

    Added Projects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,11 +14,20 @@ const About = () => {
           </div>
           <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
             <div className='sm:text-right text-4xl font-bold'>
-              <p>Hi! I'm Smaran Bhattarai, nice to meet you.</p>
+              <p>A Passionate Computer Science Student Seeking Internship Opportunities To Expand Skills In Software Development and Innovation.</p>
             </div>
             <div>
-              <p>I am 21-years old,originally from Nepal, currently based on Kentucky, United States. I am a third year student at Northern Kentucky University majoring
-                in Computer Science with minor in Information Technology. I am currently learnig backend (node js) and exploring full stack web developemt </p>  
+              <p>I am 21-years old ,originally from Nepal, currently based on Kentucky, United States. I am a third year student at Northern Kentucky University majoring
+                in Computer Science with minor in Information Technology. I am currently learnig backend (node js) and exploring full stack web developemt. I 
+                love watching and playing Soccer. I am a big Cristiano Ronaldo & Real Madrid fan. One fun fact about me is that I can play 
+                guitar. <br/>  <br/>I was introduced with HTML and CSS in my junior year of high school. In my senior year, I was introduced
+                with 'C' programming language where I wrote my first 'Hello World' statement. After my high school graduation, I spent time learning 
+                Python and concepts like Object-Oriented Programming (using Python). Then I was introduced with Java in my college and got 
+                chance to learn concepts like Data-Structues & Algorithm (using Java). In the middle of my Sophomore year, I got interest in developemnt
+                and started learning web development technologies. Since, I had experience of HTMl/CSS and base of JavaScript, I started 
+                learnig React framework and making front-end projects. After having good base of React, I am currently mastering backend learning
+                node, express frame-work and Mongodb.
+                   </p>  
             </div>
           </div>
       </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,8 @@
   import workImg from '../assets/projects/workImg.jpg'
   import Dice from '../assets/projects/dice.jpg'
   import Weather from '../assets/projects/weather.jpg'
+  import Blog from '../assets/projects/blog.jpg'
+  import Movie from '../assets/projects/movie.jpg'
 
   const Work = () => {
     return (
@@ -9,13 +11,13 @@
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div  className='pb-8'> 
             <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
-            <p className='py-6'>Check out some of my recent works</p>
+            <p className='py-6'>Check out some of my recent works are as follows:</p>
           </div>
 
           {/* Container*/}
           <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
 
-                       {/*Grid Item */}
+                       {/*Project 1 */}
 
                 <div style={{ backgroundImage: `url(${workImg})` }}
                 className="shadow-lg shadow-[#040c16] group container rounded-md 
@@ -34,6 +36,7 @@
                     </div>
                   </div>
                 </div>
+                {/* Project 2*/}
                 <div style={{ backgroundImage: `url(${Dice})` }}
                 className="shadow-lg shadow-[#040c16] group container rounded-md 
                     flex justify-center text-center items-center mx-auto content-div ">
@@ -63,6 +66,42 @@
                     </span>
                     <div className="pt-8 text-center ">
                       <a href='https://github.com/smaranbh7/weather-app' target='_blank'>
+                        <button className="text-center rounded-lg px-4 py-3 m-2
+                            bg-white text-gray-700 font-bold text-lg">Code</button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                {/* Project 4*/}
+                <div style={{ backgroundImage: `url(${Blog})` }}
+                className="shadow-lg shadow-[#040c16] group container rounded-md 
+                    flex justify-center text-center items-center mx-auto content-div ">
+                  
+                  {/*Hover Effect */}
+                  <div className="opacity-0 group-hover:opacity-100 ">
+                    <span className="text-2xl font bold text-white tracking-wider ">
+                        Blog Post Application
+                    </span>
+                    <div className="pt-8 text-center ">
+                      <a href='https://github.com/smaranbh7/The-Blog-Wall' target='_blank'>
+                        <button className="text-center rounded-lg px-4 py-3 m-2
+                            bg-white text-gray-700 font-bold text-lg">Code</button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                {/* Project 5*/}
+                <div style={{ backgroundImage: `url(${Movie})` }}
+                className="shadow-lg shadow-[#040c16] group container rounded-md 
+                    flex justify-center text-center items-center mx-auto content-div ">
+                  
+                  {/*Hover Effect */}
+                  <div className="opacity-0 group-hover:opacity-100 ">
+                    <span className="text-2xl font bold text-white tracking-wider ">
+                        Movie Application
+                    </span>
+                    <div className="pt-8 text-center ">
+                      <a href='https://github.com/smaranbh7/Movie-app' target='_blank'>
                         <button className="text-center rounded-lg px-4 py-3 m-2
                             bg-white text-gray-700 font-bold text-lg">Code</button>
                       </a>
