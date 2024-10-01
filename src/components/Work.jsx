ff5b2dc3d744191507edf821cf76da7885ad811e
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-09-30 21:15:55 -0400

    typos

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -11,7 +11,7 @@
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div  className='pb-8'> 
             <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
-            <p className='py-6'>Check out some of my recent works are as follows:</p>
+            <p className='py-6'>Check out some of my recent works below:</p>
           </div>
 
           {/* Container*/}
