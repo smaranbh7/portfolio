cd7f527d03224999d6cf58a58e69e400c976a13b
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-09-30 21:19:05 -0400

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import {
   FaLinkedin
 } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
+
 //import Logo from '../assets/logo.png';
 import { Link } from 'react-scroll';
 
