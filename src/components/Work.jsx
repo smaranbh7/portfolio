db0de96601db316f18a54e82ea902bcc4e4e348a
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-10-12 02:50:25 -0400

    new project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,6 +4,7 @@
   import Weather from '../assets/projects/weather.jpg'
   import Blog from '../assets/projects/blog.jpg'
   import Movie from '../assets/projects/movie.jpg'
+  import mockProject from '../assets/projects/mockProject.jpg'
 
   const Work = () => {
     return (
@@ -16,6 +17,28 @@
 
           {/* Container*/}
           <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/*New Project */}
+          <div style={{ backgroundImage: `url(${mockProject})` }}
+                className="shadow-lg shadow-[#040c16] group container rounded-md 
+                    flex justify-center text-center items-center mx-auto content-div ">
+                  
+                  {/*Hover Effect */}
+                  <div className="opacity-0 group-hover:opacity-100 ">
+                    <span className="text-2xl font bold text-white tracking-wider ">
+                        Ask Me (AI-powered Mock Interview Potal)
+                    </span>
+                    <div className="pt-8 text-center ">
+                      <a href='https://github.com/smaranbh7/AI-Powered-Mock-Interview-Portal' rel="noopener noreferrer">
+                        <button className="text-center rounded-lg px-4 py-3 m-2
+                            bg-white text-gray-700 font-bold text-lg">Code</button>
+                      </a>
+                      <a href='https://ai-powred-mock-interview-portal.vercel.app/' rel="noopener noreferrer">
+                        <button className="text-center rounded-lg px-4 py-3 m-2
+                            bg-white text-gray-700 font-bold text-lg">Demo</button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
 
                        {/*Project 1 */}
 
