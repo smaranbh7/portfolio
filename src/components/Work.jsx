34e678c93927d22ae7d542530796618cc554d6be
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-11-16 15:57:45 -0500

    broken links fix

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -28,7 +28,7 @@
                         Ask Me (AI-powered Mock Interview Potal)
                     </span>
                     <div className="pt-8 text-center ">
-                      <a href='https://github.com/smaranbh7/AI-Powered-Mock-Interview-Portal' rel="noopener noreferrer">
+                      <a href='https://ask-me-delta-ten.vercel.app/' rel="noopener noreferrer">
                         <button className="text-center rounded-lg px-4 py-3 m-2
                             bg-white text-gray-700 font-bold text-lg">Code</button>
                       </a>
